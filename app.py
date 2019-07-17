4725fb436cbd76f5f6d99d764c23b81bb7fc5938
Sewi1808 <szalasny.s@gmail.com>
2019-07-17 22:33:46 +0200

    create post api for /add_two_nums view

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,20 @@ def json_test():
 		'key2':'value2'
 	}
 	return jsonify(someJson)
+
+@app.route('/add_two_nums', methods=["POST"])
+def add_two_nums():
+	# get x y from post
+	dataDict = request.get_json()
+	x = dataDict["x"]
+	y = dataDict["y"]
+	#add x + y =z
+	z = x + y
+	retJSON = {
+		"z" : z
+	}
+	#return jsonify(map_prepared)
+	return jsonify(retJSON), 200
 
 if __name__=="__main__":
 	app.run(debug=True)
