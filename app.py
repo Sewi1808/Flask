82e89f2ef5a8a695561b0ce4865923df1796d455
Sewi1808 <szalasny.s@gmail.com>
2019-07-17 20:51:49 +0200

    add jsonify and /json view

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return "Hello_world!"
 
+@app.route('/json')
+def json_test():
+	someJson = {
+		'key1':'value1' 
+		'key2':'value2'
+	}
+	return jsonify(someJson)
+
 if __name__=="__main__":
-	app.run()
+	app.run(debug=True)
